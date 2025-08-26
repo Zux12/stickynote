eb5257e758950c8962c709111b52d8a18627c454
Zux12 <158550944+Zux12@users.noreply.github.com>
2025-08-26 13:37:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname, { extensions: ['html'] }));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'notes.html')); // or index.html if you renamed it
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
